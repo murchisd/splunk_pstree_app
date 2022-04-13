722fab40361f9369b77f94670a4530ff35fef067
Donald Murchison <donemurch@gmail.com>
2022-04-13 15:56:49 -0700

    Added command pstree_v2 which uses iterative algorithm for evaluation

--- splunk_pstree_app/bin/pstree_v2.py
+++ splunk_pstree_app/bin/pstree_v2.py
@@ -15,12 +15,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-#Ps tree logic copied from Giampaolo Rodola's gitub
-# Copyright (c) 2009, Giampaolo Rodola'. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#https://github.com/giampaolo
-#https://github.com/giampaolo/psutil/blob/master/scripts/pstree.py
+# pstree logic adapted from 
+# Iterative Preorder Traversal of an N-ary Tree
+# https://www.geeksforgeeks.org/iterative-preorder-traversal-of-a-n-ary-tree/#:~:text=Preorder%20Traversal%20of%20an%20N%2Dary%20Tree%20is%20similar%20to,Preorder%20Traversal%20of%20Binary%20Tree.
 
 
 
@@ -31,7 +28,7 @@ sys.path.append(os.path.join(splunkhome, 'etc', 'apps', 'splunk_pstree_app', 'li
 from splunklib.searchcommands import dispatch, EventingCommand, Configuration, Option, validators
 
 @Configuration()
-class PSTreeCommand(EventingCommand):
+class PSTreeCommand_V2(EventingCommand):
         """ Displays a tree structure using a parent and child field from splunk logs. Intended for sysmon EventCode=1 events
         but can be used for anything. App will return a multivalue field for each root process displaying all children
         processes in tree format.
@@ -66,44 +63,28 @@ class PSTreeCommand(EventingCommand):
         **Syntax:** **child=***<fieldname>*
         **Description:** Name of the field that holds the child value''',
         require=True, validate=validators.Fieldname())
-        
+
         detail = Option(
         doc='''
         **Syntax:** **detail=***<fieldname>*
         **Description:** Name of the field that holds detail value for child field''',
         require=False, validate=validators.Fieldname())
-        
+
         spaces = Option(
         doc='''
         **Syntax:** **tabs=***int*
         **Description:** Name of the field that holds detail value for child field''',
         require=False, validate=validators.Integer())
 
-        def make_tree(self,parent,details, tree, indent, return_array, prefix, spaces):
-            #Adjust number of spaces to keep consistently spaced column for details
-            space=" "
-            if (len(parent)+len(prefix)) < spaces:
-                space=space*(spaces-len(prefix)-len(parent))
-            # Append line for process in tree
-            return_array.append(prefix+parent+space+details)
-            # Check to see if current process had any children 
-            if parent not in tree:
-                    return
-            #For every child process recursively build tree
-            children = tree[parent].keys()
-            for child in children: 
-                self.make_tree(child, tree[parent][child],tree, indent + "`` ",return_array, indent + "|--- ",spaces)
-
-
-
         def transform(self, records):
-                self.logger.debug('PSTreeCommand: %s', self)  
+                self.logger.debug('PSTreeCommand: %s', self)
                 #initialize tree structure as dictionary of dictionaries (updated to dict of dict to allow passing of details)
                 tree=collections.defaultdict(lambda: collections.defaultdict(str))
                 #initialize array to keep track of children processes
                 children=[]
                 #Set default spaces to 120
                 spaces=120
+                indent="`` "
                 if self.spaces:
                     spaces=self.spaces
                 # For every event add parent as key in outer dict and child as key in nested dict
@@ -115,12 +96,41 @@ class PSTreeCommand(EventingCommand):
                         tree[record[self.parent]][record[self.child]]=""
                     #Add child to array to be able find root of pstree - every process associated with an EventCode 1 will be in this array
                     children.append(record[self.child])
+                self.logger.error("PSTREE %s",tree)
                 for parent in tree:
                     #For every parent check if in children array - only Parent Processes with no Process Creation event(Event Code 1) will match this criteria
                     if parent not in children:
-                            tmp=[]
-                            #Recursively build tree for every root process
-                            self.make_tree(parent,'',tree,'',tmp,'',spaces)
-                            yield {"tree":tmp}
+                        stack=collections.deque([])
+                        branches=[]
+                        preorder=[]
+                        branches.append(parent)
+                        preorder.append(parent)
+                        stack.append(parent)
+                        depth=-1
+                        while len(stack)>0:
+                            flag=0
+                            if stack[len(stack)-1] not in tree.keys():
+                                stack.pop()
+                                depth=depth-1
+                            else:
+                                parent=stack[len(stack)-1]
+                            for child in tree[parent].keys():
+                                if child not in preorder:
+                                    flag=1
+                                    depth=depth+1
+                                    stack.append(child)
+                                    preorder.append(child)
+                                    prefix=(indent*depth)+"|--- "
+                                    space=" "
+                                    if (len(child)+len(prefix)) < spaces:
+                                        space=space*(spaces-len(prefix)-len(child))
+                                    branches.append(prefix+c+space+tree[parent][child])
+                                    break;
+                            if flag==0:
+                                stack.pop()
+                                depth=depth-1
 
-dispatch(PSTreeCommand, sys.argv, sys.stdin, sys.stdout, __name__)
+
+                        yield {"tree":branches}
+
+dispatch(PSTreeCommand_V2, sys.argv, sys.stdin, sys.stdout, __name__)
